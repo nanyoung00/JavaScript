09a9a8835fb32acbc84697c76f148862900cc1c4
nanyoung00 <nyungnim00@gmail.com>
2022-12-08 17:15:35 +0900

    todo list 파트 진도 나가는 중

--- nomadChormeApp/js/todo.js
+++ nomadChormeApp/js/todo.js
@@ -2,18 +2,49 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); // string으로 저장
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "✖️";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li); // append는 뒤에 있어야한다
 }
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
-    toDoInput.value = "";
+    toDoInput.value = " "; //이거 칸 띄워야하는거 실화냐
+    toDos.push(newTodo); // 이걸 로컬저장소에 넣는게 목표
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello() {
+    console.log("hello");
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+}
